0b44a54ca2e831ccdc1b6e4944ea3dab99a6694e
Naomikho <khoyining@proton.me>
2025-12-30 21:14:48 +0800

    wip: add class and fix the ts warning

---
+++ src/models/UmaCircleModel.ts
@@ -0,0 +1,19 @@
+export default class UmaCircleModel {
+    private circleId: number;
+    private name: string;
+    private comment: string | null;
+    private leaderViewerId: number;
+    private leaderName: string;
+    private memberCount: number;
+    private joinStyle: number;
+    private policy: number;
+    private createdAt: string;
+    private lastUpdated: string;
+    private monthlyRank: number;
+    private monthlyPoint: number;
+    private lastMonthRank: number;
+    private lastMonthPoint: number;
+    private archived: boolean;
+    private yesterdayUpdated: string;
+    private yesterdayRank: number;
+}
